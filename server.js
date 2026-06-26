17952c81a2abf3279564eb4cd025203f0b95b937
Promise Duke <promiseduke@gmail.com>
2026-06-26 17:58:42 +0100

    Adding cron job to activate/ping the server from inactivity

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { createServer } from "node:http";
 import next from "next";
 import { Server } from "socket.io";
-import job from "./lib/cron"; // no .ts extension
+import job from "./lib/cron.js";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
