700ec724b3ce260908f36b7a4bd43d0d5556278c
Promise Duke <promiseduke@gmail.com>
2026-07-03 09:42:42 +0100

    nav name

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -54,7 +54,7 @@ const NavBar = () => {
               className="h-10 w-10 object-contain hidden dark:block"
               priority
             /> */}
-            <div className="font-bold text-xl">CHAMPIONS PATH</div>
+            <div className="font-bold text-xl">TECH PATH</div>
           </div>
           {isFeedsPage && <SearchInput />}
           <div className="flex gap-5 sm:gap-8 items-center">
