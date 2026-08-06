accbf0e530c9854f0a1924a7a94a405e99902b32
Promise Duke <promiseduke@gmail.com>
2026-08-06 17:59:50 +0100

    Dashboard/cards mobile fix11

--- components/dashboard/UserDashboard.tsx
+++ components/dashboard/UserDashboard.tsx
@@ -80,7 +80,7 @@ const UserDashboard = ({
       <div className="grid gap-4 lg:grid-cols-[1.4fr_0.95fr]">
         <UserDashboardRecent posts={recentPosts} />
         <div className="w-full min-w-0 flex flex-col gap-4">
-          <div className="w-full min-w-0 overflow-hidden pb-2">
+          <div className="w-full min-w-0 overflow-x-auto pb-2">
             <UserDashboardChart
               weeklyViews={weeklyViews}
               topPosts={topViewedPosts}
