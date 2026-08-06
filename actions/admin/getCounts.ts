1b52548cbf658aa2ccd7700b65bc021853c1810e
Promise Duke <promiseduke@gmail.com>
2026-08-06 11:30:37 +0100

    adding more dashboard features for users/adminX3

--- actions/admin/getCounts.ts
+++ actions/admin/getCounts.ts
@@ -29,7 +29,7 @@ export const getCounts = async () => {
         totalClaps,
         totalComments,
         totalBookmarks,
-        totalViews: viewAggregate._sum.views ?? 0,
+        totalViews: viewAggregate._sum?.views ?? 0,
       },
     };
   } catch (error) {
